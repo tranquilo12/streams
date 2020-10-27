d73a00a7cf070696c3ff16db0bdbb77afbde8c89
Tranquilo12 <shriram.sunder121091@gmail.com>
2020-10-27 21:34:05 +0400

    **** multi_thread_streams.py **** - new functions process_stocks_equities_aggregates_for_redis, process_stocks_equities_aggregates_for_postgres, push_into_redis_cache, pull_from_redis_cache, parse_redis_output_for_postgres
    - all logic within download_and_push_into_db has been moved into a try, except clause

--- multi_thread_streams.py
+++ multi_thread_streams.py
@@ -1,14 +1,19 @@
+from connections import Connections
 from polygon import RESTClient
 from threading import Thread
-from connections import Connections
 from tqdm.auto import tqdm
 from typing import Optional
 from queue import Queue
 import pandas as pd
 import numpy as np
+import functools
+import sshtunnel
 import requests
 import psycopg2
 import datetime
+import redis
+import pickle
+import json
 
 
 class Worker(Thread):
@@ -47,10 +52,7 @@ class ThreadPool:
 
 
 def insert_into_db(
-    db_conn_params: dict,
-    batched: list,
-    query_template: str,
-    ticker: str,
+    db_conn_params: dict, batched: list, query_template: str, ticker: str,
 ):
     """
     Take the stuff from process_stocks_equities_aggregates and push it ino the database
@@ -72,9 +74,9 @@ def insert_into_db(
                     pass
 
 
-def establish_ssh_tunnel(ssh_conn_params: dict) -> SSHTunnelForwarder:
+def establish_ssh_tunnel(ssh_conn_params: dict) -> sshtunnel.SSHTunnelForwarder:
     try:
-        t = SSHTunnelForwarder(**ssh_conn_params)
+        t = sshtunnel.SSHTunnelForwarder(**ssh_conn_params)
     except (
         sshtunnel.BaseSSHTunnelForwarderError,
         sshtunnel.HandlerSSHTunnelForwarderError,
@@ -132,7 +134,51 @@ def request_stocks_equities_aggregates(
     return records
 
 
-def process_stocks_equities_aggregates(
+def process_stocks_equities_aggregates_for_redis(
+    records: Optional[requests.Response], ticker: str, timespan: str, multiplier: int,
+) -> [list, dict]:
+    """
+    Make sure it's easy to decipher the processing, by separating the logic
+    :param records:
+    :param ticker:
+    :param timespan:
+    :param multiplier:
+    :return:
+    """
+    results = records.results
+    if results is None:
+        return None, None
+
+    try:
+        keys = [
+            f"{ticker}_{timespan}_{multiplier}_{result['vw']}_{result['t']}"
+            for result in results
+        ]
+    except Exception as e:
+        keys = None
+        print(f"Dont know exception location 1.1: {e}")
+
+    try:
+        new_results = {
+            f"{ticker}_{timespan}_{multiplier}_{result['vw']}_{result['t']}": json.dumps(
+                {
+                    "volume": result["v"],
+                    "open": result["o"],
+                    "close": result["c"],
+                    "high": result["h"],
+                    "low": result["l"],
+                }
+            )
+            for result in results
+        }
+    except Exception as e:
+        new_results = None
+        print(f"Dont know exception location 1.2: {e}")
+
+    return keys, new_results
+
+
+def process_stocks_equities_aggregates_for_postgres(
     records: Optional[requests.Response], ticker: str, timespan: str, multiplier: int,
 ) -> [list, str]:
     """
@@ -144,7 +190,7 @@ def process_stocks_equities_aggregates(
     :return:
     """
 
-    historic_agg_columns: dict = {
+    historic_agg_columns = {
         "v": "volume",
         "vw": "vwap",
         "o": "open",
@@ -159,7 +205,6 @@ def process_stocks_equities_aggregates(
         try:
             df_ = pd.DataFrame.from_records(records.results)
         except TypeError as e:
-            # print(f"TypeError: {e}")
             return None, None
 
         df_ = df_.rename(columns=historic_agg_columns)
@@ -181,7 +226,6 @@ def process_stocks_equities_aggregates(
         try:
             batched = [b for b in batch(values, n=batch_size)]
         except ValueError as e:
-            # print(f"ValueError: {e}")
             return None, None
 
         # make te query template for this query, that will be inserted into the table
@@ -195,11 +239,76 @@ def process_stocks_equities_aggregates(
     return batched, query_template
 
 
+def push_into_redis_cache(redis_client: redis.Redis, encoded_records: dict) -> None:
+    """
+    Take the response from request_stocks_equities_agg and
+    push into the redis cache
+    :param encoded_records:
+    :param redis_client:
+    :return:
+    """
+    for key, val in encoded_records.items():
+        try:
+            redis_client.set(key, val)
+        except Exception as e:
+            print(f"Exception while inserting to redis: {e}")
+
+
+def pull_from_redis_cache(redis_client: redis.Redis, keys_list: list) -> dict:
+    results = {}
+    for key in keys_list:
+        try:
+            results[key] = redis_client.get(name=key)
+        except Exception as e:
+            print(f"Exception while fetching data from redis: {e}")
+
+    return results
+
+
+def parse_redis_output_for_postgres(results: dict) -> [list, str]:
+    """
+    Take the output from 'pull_from_redis_cache', an
+    :param results:
+    :return:
+    """
+    all_results = []
+    for key, val in results.items():
+        keys = key.split("_")
+        val = json.loads(s=val)
+        expanded_results = [
+            keys[0],
+            keys[1],
+            keys[2],
+            float(val["volume"]),
+            keys[3],
+            float(val["open"]),
+            float(val["close"]),
+            float(val["high"]),
+            float(val["low"]),
+            pd.to_datetime(keys[4], unit="ms"),
+        ]
+        all_results.append(expanded_results)
+
+    try:
+        batched = [b for b in batch(all_results, n=100)]
+    except ValueError as e:
+        return None, None
+
+    all_cols_str = (
+        "ticker, timespan, multiplier, volume, vwap, open, close, high, low, timestamp"
+    )
+    table_name = "polygon_stocks_agg_candles"
+    conflict_cols = "(ticker, timespan, multiplier, vwap, timestamp)"
+    query_template = f"INSERT INTO {table_name} ({all_cols_str}) VALUES %s ON CONFLICT {conflict_cols} DO NOTHING"
+    return batched, query_template
+
+
 def download_and_push_into_db(
     logger,
+    disable_logging: bool,
     ticker: str,
-    client: RESTClient,
-    ssh_conn_params: dict,
+    rest_client: RESTClient,
+    redis_client: redis.Redis,
     db_conn_params: dict,
     multiplier: int = 1,
     timespan: str = "day",
@@ -209,8 +318,11 @@ def download_and_push_into_db(
     """
     A wrapper function that lines all the others up
     :param ticker:
-    :param ssh_conn_params:
+    :param redis_client:
     :param db_conn_params:
+    :param rest_client:
+    :param disable_logging:
+    :param logger:
     :param multiplier:
     :param timespan:
     :param from_:
@@ -220,30 +332,65 @@ def download_and_push_into_db(
 
     records = request_stocks_equities_aggregates(
         ticker=ticker,
-        client=client,
+        client=rest_client,
         multiplier=multiplier,
         timespan=timespan,
         from_=from_,
         to=to,
     )
 
-    batched, query_template = process_stocks_equities_aggregates(
-        records=records, ticker=ticker, timespan=timespan, multiplier=multiplier,
-    )
+    # batched, query_template = process_stocks_equities_aggregates_for_postgres(
+    #     records=records, ticker=ticker, timespan=timespan, multiplier=multiplier,
+    # )
+
+    try:
+        keys, new_results = process_stocks_equities_aggregates_for_redis(
+            records=records, ticker=ticker, timespan=timespan, multiplier=multiplier
+        )
+    except Exception as e:
+        keys, new_results = None, None
+        print(f"Dont know exception location 1: {e}")
+
+    try:
+        if isinstance(new_results, dict):
+            push_into_redis_cache(
+                redis_client=redis_client, encoded_records=new_results
+            )
+    except Exception as e:
+        print(f"Dont know exception location 2: {e}")
+
+    try:
+        if isinstance(keys, list):
+            results = pull_from_redis_cache(redis_client=redis_client, keys_list=keys)
+    except Exception as e:
+        results = None
+        print(f"Dont know exception location 3: {e}")
+
+    try:
+        batched, query_template = parse_redis_output_for_postgres(results=results)
+    except Exception as e:
+        batched, query_template = None, None
+        print(f"Dont know exception location 4: {e}")
 
     if (batched is not None) & (query_template is not None):
+        logger.info(msg=f"Ticker : {ticker} Inserting to db...")
         insert_into_db(
-            ssh_conn_params=ssh_conn_params,
             db_conn_params=db_conn_params,
             query_template=query_template,
             batched=batched,
             ticker=ticker,
         )
     else:
-        logger.info(msg=f"ticker : {ticker} has returned None.")
+        if disable_logging:
+            prev_state = logger.disabled
+            logger.disabled = True
+            logger.info(msg=f"Ticker : {ticker} has returned None.")
+            logger.disabled = prev_state
+        else:
+            logger.info(msg=f"Ticker : {ticker} has returned None.")
 
 
-def get_all_equities_list(logger, db_conn_params) -> list:
+def get_all_equities_list(logger, db_conn_params: dict) -> list:
     # query the db for equities list
 
     logger.info(msg="Making ssh tunnel to get equities list...")
@@ -254,22 +401,23 @@ def get_all_equities_list(logger, db_conn_params) -> list:
                 logger.info(msg="Querying db...")
                 cur.execute(query)
                 logger.info(msg="Fetchall query...")
-                equities_list = cur.fetchall()
+                res = cur.fetchall()
 
-        equities_list = [val[0].replace(" ", "") for val in equities_list]
-        equities_list = [val for val in equities_list if ("^" not in val)]
+        res = [val[0].replace(" ", "") for val in res]
+        res = [val for val in res if ("^" not in val)]
 
     except psycopg2.OperationalError as e:
         logger.error(msg=f"Psycopg2 Op Error: {e}")
-        equities_list = None
+        res = None
 
-    return equities_list
+    return res
 
 
 if __name__ == "__main__":
 
     conns = Connections()
     conns.establish_rest_client()
+    conns.establish_redis_connection()
 
     rest_client = conns.rest_client
     ssh_conn_params = conns.ssh_conn_params
@@ -291,12 +439,13 @@ if __name__ == "__main__":
         pool.add_tasks(
             func=download_and_push_into_db,
             logger=conns.logger,
-            client=rest_client,
+            disable_logging=False,
+            rest_client=rest_client,
+            redis_client=conns.redis_client,
             ticker=eq,
-            ssh_conn_params=ssh_conn_params,
             db_conn_params=db_conn_params,
             timespan="day",
-            from_=datetime.date.today() - datetime.timedelta(days=10),
+            from_=datetime.date.today() - datetime.timedelta(days=23),
         )
 
     conns.logger.info(msg="Waiting for pool tasks to complete...")
